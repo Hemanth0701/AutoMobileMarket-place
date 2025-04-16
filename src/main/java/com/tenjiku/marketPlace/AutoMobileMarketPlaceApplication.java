7603c60b9ff75bcc3e749ff29e2d9fb8f44930f4
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-04-16 15:13:35 +0530

    intial commit

---
+++ src/main/java/com/tenjiku/marketPlace/AutoMobileMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.tenjiku.marketPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMobileMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMobileMarketPlaceApplication.class, args);
+	}
+
+}
